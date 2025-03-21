7368715ad5ddcb925cfc2cd8478684b0378804b5
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-03-21 14:25:51 +0000

    Use `T{]` casting instead of `static_cast<>`

--- glm/detail/_noise.hpp
+++ glm/detail/_noise.hpp
@@ -9,37 +9,37 @@ namespace detail
 template <typename T>
 GLM_FUNC_QUALIFIER T mod289(const T& x)
 {
-    return x - floor(x * (static_cast<T>(1.0) / static_cast<T>(289.0))) * static_cast<T>(289.0);
+    return x - floor(x * (T{1} / T{289})) * T{289};
 }
 
 template <typename T>
 GLM_FUNC_QUALIFIER T permute(const T& x)
 {
-    return mod289(((x * static_cast<T>(34)) + static_cast<T>(1)) * x);
+    return mod289(((x * T{34}) + T{1}) * x);
 }
 
 template <length_t L, typename T, qualifier Q>
 GLM_FUNC_QUALIFIER vec<L, T, Q> permute(const vec<L, T, Q>& x)
 {
-    return mod289(((x * static_cast<T>(34)) + static_cast<T>(1)) * x);
+    return mod289(((x * T{34}) + T{1}) * x);
 }
 
 template <typename T>
 GLM_FUNC_QUALIFIER T taylorInvSqrt(const T& r)
 {
-    return static_cast<T>(1.79284291400159) - static_cast<T>(0.85373472095314) * r;
+    return T{1.79284291400159} - T{0.85373472095314} * r;
 }
 
 template <length_t L, typename T, qualifier Q>
 GLM_FUNC_QUALIFIER vec<L, T, Q> taylorInvSqrt(const vec<L, T, Q>& r)
 {
-    return static_cast<T>(1.79284291400159) - static_cast<T>(0.85373472095314) * r;
+    return T{1.79284291400159} - T{0.85373472095314} * r;
 }
 
 template <length_t L, typename T, qualifier Q>
 GLM_FUNC_QUALIFIER vec<L, T, Q> fade(const vec<L, T, Q>& t)
 {
-    return (t * t * t) * (t * (t * static_cast<T>(6) - static_cast<T>(15)) + static_cast<T>(10));
+    return (t * t * t) * (t * (t * T{6} - T{15}) + T{10});
 }
 
 } //namespace detail
